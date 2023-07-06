2154eeb550d687e679d14913f2f49882e9044002
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-07-06 07:57:34 +0300

    setup the app

---
+++ lib/constants/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Maincolors {
+  static var primaryColor = const Color(0xff296e48);
+}
---
+++ lib/constants/string.dart
@@ -0,0 +1,9 @@
+class Allstrings {
+  static var titleone = "";
+  static var descriptionone = "";
+  static var titletwo = "";
+  static var descriptiontwo = "";
+  static var titlethree = "";
+  static var descriptionthree = "";
+
+}
